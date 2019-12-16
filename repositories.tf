225dab51a4878b0361cf54d94ddda55677ec91b6
VEBER Arnaud <arnaud@veber.pw>
2019-12-16 23:07:57 +0100

    feat(tf,repositories): repositories management

---
+++ repositories.tf
@@ -0,0 +1,44 @@
+module "github" {
+  source = "./module/repository/"
+
+  # repository
+  repository-name        = "github"
+  repository-description = "github organization management with terraform"
+  repository-topics      = ["terraform", "github"]
+
+  repository-private = false
+
+  repository-auto_init      = false
+  repository-default_branch = "master"
+
+  # branches protection
+  branches_protection = [
+    {
+      branch                                     = "master"
+      enforce_admins                             = true
+      require_signed_commits                     = true
+      status_check-strict                        = true
+      status_check-contexts                      = ["continuous-integration/travis-ci"]
+      pr_reviews-required_approving_review_count = 1
+      pr_reviews-require_code_owner_reviews      = true
+      pr_reviews-dismiss_stale_reviews           = true
+      pr_reviews-dismissal_users                 = []
+      pr_reviews-dismissal_teams                 = [module.core.slug]
+      restrictions-users                         = []
+      restrictions-teams                         = []
+    }
+  ]
+
+  # webhooks
+  webhooks = [
+    {
+      url          = "https://notify.travis-ci.org"
+      content_type = "form"
+      insecure_ssl = false
+      active       = true
+      events       = ["create", "delete", "issue_comment", "member", "public", "pull_request", "push", "repository"]
+    }
+  ]
+
+  repository-issue_labels = concat(var.default-issue_labels)
+}
