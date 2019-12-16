d3e67876e67730e2765a441fa84c0f13b2347edf
VEBER Arnaud <arnaud@veber.pw>
2019-12-16 23:07:17 +0100

    feat(tf,users): users management

---
+++ users.tf
@@ -0,0 +1,505 @@
+# AMAR Benjamin
+module "AMARBenjamin" {
+  source = "./module/user/"
+
+  user-name = "BenAmarEleven"
+  user-role = "member"
+}
+
+# ANDRÉ Alexandre
+module "ANDREAlexandre" {
+  source = "./module/user/"
+
+  user-name = "alex-ception"
+  user-role = "member"
+}
+
+# BERNARD Jeremy
+module "BERNARDJeremy" {
+  source = "./module/user/"
+
+  user-name = "BernardJeremy"
+  user-role = "member"
+}
+
+# BERRY Elsa
+module "BERRYElsa" {
+  source = "./module/user/"
+
+  user-name = "ElsaEleven"
+  user-role = "member"
+}
+
+# BERTRAND Vincent
+module "BERTRANDVincent" {
+  source = "./module/user/"
+
+  user-name = "Vubs"
+  user-role = "member"
+}
+
+# BILLARD MADRIERES Victor
+module "BILLARDMADRIERESVictor" {
+  source = "./module/user/"
+
+  user-name = "vbillardm"
+  user-role = "member"
+}
+
+# BOE Hadrien
+module "BOEHadrien" {
+  source = "./module/user/"
+
+  user-name = "Katario"
+  user-role = "member"
+}
+
+# BOILLON Yannick
+module "BOILLONYannick" {
+  source = "./module/user/"
+
+  user-name = "yannickBoillon"
+  user-role = "member"
+}
+
+# BRENOT Pierre
+module "BRENOTPierre" {
+  source = "./module/user/"
+
+  user-name = "flipper-ananas"
+  user-role = "member"
+}
+
+# BRUGIDOU Thibaut
+module "BRUGIDOUThibaut" {
+  source = "./module/user/"
+
+  user-name = "Thibaut-Brugidou"
+  user-role = "member"
+}
+
+# CADOUX Martin
+module "CADOUXMartin" {
+  source = "./module/user/"
+
+  user-name = "martincadoux"
+  user-role = "member"
+}
+
+# CALONE Bastien
+module "CALONEBastien" {
+  source = "./module/user/"
+
+  user-name = "BastienCalone"
+  user-role = "member"
+}
+
+# CANAL Guillem
+module "CANALGuillem" {
+  source = "./module/user/"
+
+  user-name = "guillemcanal"
+  user-role = "member"
+}
+
+# CHARRON Maeva
+module "CHARRONMaeva" {
+  source = "./module/user/"
+
+  user-name = "mcharron11labs"
+  user-role = "member"
+}
+
+# CLAVIER Anais
+module "CLAVIERAnais" {
+  source = "./module/user/"
+
+  user-name = "Anacla"
+  user-role = "member"
+}
+
+# CORDIER Kevin
+module "CORDIERKevin" {
+  source = "./module/user/"
+
+  user-name = "frisoty"
+  user-role = "member"
+}
+
+# DELPEYROUX Victor
+module "DELPEYROUXVictor" {
+  source = "./module/user/"
+
+  user-name = "vdelpeyroux"
+  user-role = "member"
+}
+
+# DIOURI Younes
+module "DIOURIYounes" {
+  source = "./module/user/"
+
+  user-name = "younesdiouri"
+  user-role = "member"
+}
+
+# DRUON Mehdi
+module "DRUONMehdi" {
+  source = "./module/user/"
+
+  user-name = "MehdiDr"
+  user-role = "member"
+}
+
+# DURAND Hugo
+module "DURANDHugo" {
+  source = "./module/user/"
+
+  user-name = "HugoDurand"
+  user-role = "member"
+}
+
+# DURAND Romain
+module "DURANDRomain" {
+  source = "./module/user/"
+
+  user-name = "romaindurand"
+  user-role = "member"
+}
+
+# EINHORN Stephane
+module "EINHORNStephane" {
+  source = "./module/user/"
+
+  user-name = "seinhorn"
+  user-role = "member"
+}
+
+# EL FARSAOUI Kamal
+module "ELFARSAOUIKamal" {
+  source = "./module/user/"
+
+  user-name = "elfakamal"
+  user-role = "member"
+}
+
+# FRUIT Dimitri
+module "FRUITDimitri" {
+  source = "./module/user/"
+
+  user-name = "DimitriMorgan"
+  user-role = "member"
+}
+
+# GORRON Charles Eric
+module "GORRONCharlesEric" {
+  source = "./module/user/"
+
+  user-name = "ch3ric"
+  user-role = "admin"
+}
+
+# GRAILLON Robin
+module "GRAILLONRobin" {
+  source = "./module/user/"
+
+  user-name = "graillus"
+  user-role = "member"
+}
+
+# GREAUX Jeremy
+module "GREAUXJeremy" {
+  source = "./module/user/"
+
+  user-name = "JeremyGreaux"
+  user-role = "member"
+}
+
+# GREVIN Nicolas
+module "GREVINNicolas" {
+  source = "./module/user/"
+
+  user-name = "nicolas-grevin"
+  user-role = "member"
+}
+
+# HUCHON Thibaud
+module "HUCHONThibaud" {
+  source = "./module/user/"
+
+  user-name = "ettibo"
+  user-role = "member"
+}
+
+# JACQUEMIN Arthur
+module "JACQUEMINArthur" {
+  source = "./module/user/"
+
+  user-name = "ArthurJCQ"
+  user-role = "member"
+}
+
+# JARDINET Remy
+module "JARDINETRemy" {
+  source = "./module/user/"
+
+  user-name = "Pouzor"
+  user-role = "admin"
+}
+
+# KANIA Noel
+module "KANIANoel" {
+  source = "./module/user/"
+
+  user-name = "snroki"
+  user-role = "member"
+}
+
+# KPOTOGBEY-MAKU Shalom
+module "KPOTOGBEYMAKUShalom" {
+  source = "./module/user/"
+
+  user-name = "shalomaku"
+  user-role = "member"
+}
+
+# LANAU Gregory
+module "LANAUGregory" {
+  source = "./module/user/"
+
+  user-name = "glanau"
+  user-role = "member"
+}
+
+# LENGLET Martin
+module "LENGLETMartin" {
+  source = "./module/user/"
+
+  user-name = "ulrik59"
+  user-role = "member"
+}
+
+# LEROUGE Pierre
+module "LEROUGEPierre" {
+  source = "./module/user/"
+
+  user-name = "RedPi"
+  user-role = "member"
+}
+
+# MAIREAUX Maxence
+module "MAIREAUXMaxence" {
+  source = "./module/user/"
+
+  user-name = "flemzord"
+  user-role = "member"
+}
+
+# MASCLEF Romain
+module "MASCLEFRomain" {
+  source = "./module/user/"
+
+  user-name = "rmasclef"
+  user-role = "member"
+}
+
+# MAVILLAZ Remi
+module "MAVILLAZRemi" {
+  source = "./module/user/"
+
+  user-name = "KizeRemi"
+  user-role = "member"
+}
+
+# MIGEOT DE BARAN Fabrice
+module "MIGEOTDEBARANFabrice" {
+  source = "./module/user/"
+
+  user-name = "nmss"
+  user-role = "member"
+}
+
+# MINASYAN Marie
+module "MINASYANMarie" {
+  source = "./module/user/"
+
+  user-name = "MarieMinasyan"
+  user-role = "member"
+}
+
+# MOATI Jacques
+module "MOATIJacques" {
+  source = "./module/user/"
+
+  user-name = "Jmoati"
+  user-role = "member"
+}
+
+# MOUTTE Anthony
+module "MOUTTEAnthony" {
+  source = "./module/user/"
+
+  user-name = "instabledesign"
+  user-role = "member"
+}
+
+# OGREN Carl
+module "OGRENCarl" {
+  source = "./module/user/"
+
+  user-name = "RasCarlito"
+  user-role = "member"
+}
+
+# PASQUET Fabien
+module "PASQUETFabien" {
+  source = "./module/user/"
+
+  user-name = "fpasquet"
+  user-role = "member"
+}
+
+# PEAUDECERF Marion
+module "PEAUDECERFMarion" {
+  source = "./module/user/"
+
+  user-name = "MarionPdcf"
+  user-role = "member"
+}
+
+# PEJOUT Thomas
+module "PEJOUTThomas" {
+  source = "./module/user/"
+
+  user-name = "ElevenTom"
+  user-role = "admin"
+}
+
+# PIERLOT Romain
+module "PIERLOTRomain" {
+  source = "./module/user/"
+
+  user-name = "GusTTShowbiz"
+  user-role = "member"
+}
+
+# SAILLARD Damien
+module "SAILLARDDamien" {
+  source = "./module/user/"
+
+  user-name = "daemon1981"
+  user-role = "admin"
+}
+
+# SAILLARD Damien
+module "SAILLARDDamien11" {
+  source = "./module/user/"
+
+  user-name = "dsaillard-eleven-labs"
+  user-role = "admin"
+}
+
+# SOLOMON Emmanuel
+module "SOLOMONEmmanuel" {
+  source = "./module/user/"
+
+  user-name = "manuEleven"
+  user-role = "member"
+}
+
+# SUR Pierre
+module "SURPierre" {
+  source = "./module/user/"
+
+  user-name = "psur-eleven"
+  user-role = "member"
+}
+
+# THUON Thierry
+module "THUONThierry" {
+  source = "./module/user/"
+
+  user-name = "lepiaf"
+  user-role = "member"
+}
+
+# VEBER Arnaud
+module "VEBERArnaud" {
+  source = "./module/user/"
+
+  user-name = "VEBERArnaud"
+  user-role = "admin"
+}
+
+# VERMEIL Pierre
+module "VERMEILPierre" {
+  source = "./module/user/"
+
+  user-name = "pvermeil11"
+  user-role = "member"
+}
+
+# WILSON
+module "WILSON" {
+  source = "./module/user/"
+
+  user-name = "ElevenWilson"
+  user-role = "member"
+}
+
+# TODO: users to remove from organisation
+
+# AGIRI Kevin
+module "AGIRIKevin" {
+  source = "./module/user/"
+
+  user-name = "Sokrix"
+  user-role = "member"
+}
+
+# CHMITI Safia
+module "CHMITISafia" {
+  source = "./module/user/"
+
+  user-name = "Safia11"
+  user-role = "member"
+}
+
+# DARA Mehdy
+module "DARAMehdy" {
+  source = "./module/user/"
+
+  user-name = "zckrs"
+  user-role = "member"
+}
+
+# DOUCET Marion
+module "DOUCETMarion" {
+  source = "./module/user/"
+
+  user-name = "mariondou"
+  user-role = "member"
+}
+
+# JALOUZOT Jonathan
+module "JALOUZOTJonathan" {
+  source = "./module/user/"
+
+  user-name = "CaptainJojo"
+  user-role = "admin"
+}
+
+# MONCADE Cedric
+module "MONCADECedric" {
+  source = "./module/user/"
+
+  user-name = "crashbtz"
+  user-role = "member"
+}
+
+# MORENO Jean Francois
+module "MORENOJeanFrancois" {
+  source = "./module/user/"
+
+  user-name = "jiefmoreno"
+  user-role = "member"
+}
