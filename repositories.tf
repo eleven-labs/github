09b729497da5e6479aa6c52a73e3dca235eab3a2
VEBER Arnaud <arnaud@veber.pw>
2019-12-18 22:50:30 +0100

    feat(tf): import codelabs repository

--- repositories.tf
+++ repositories.tf
@@ -96,3 +96,55 @@ module "blog_eleven-labs_com" {
 
   repository-issue_labels = concat(var.default-issue_labels)
 }
+
+module "codelabs" {
+  source = "./module/repository/"
+
+  # repository
+  repository-name = "codelabs"
+
+  repository-private = false
+
+  repository-auto_init      = false
+  repository-default_branch = "master"
+
+  # branches protection
+  branches_protection = [
+    {
+      branch                                     = "master"
+      enforce_admins                             = false
+      require_signed_commits                     = false
+      status_check-strict                        = true
+      status_check-contexts                      = ["ci/circleci: node8"]
+      pr_reviews-required_approving_review_count = 1
+      pr_reviews-require_code_owner_reviews      = false
+      pr_reviews-dismiss_stale_reviews           = false
+      pr_reviews-dismissal_users                 = []
+      pr_reviews-dismissal_teams                 = []
+      restrictions-users                         = [module.JALOUZOTJonathan.login]
+      restrictions-teams                         = []
+    }
+  ]
+
+  # webhooks
+  webhooks = [
+    {
+      url          = "https://notify.travis-ci.org"
+      secret       = null
+      content_type = "form"
+      insecure_ssl = false
+      active       = true
+      events       = ["create", "delete", "issue_comment", "member", "public", "pull_request", "push", "repository"]
+    },
+    {
+      url          = "https://circleci.com/hooks/github"
+      secret       = null
+      content_type = "form"
+      insecure_ssl = false
+      active       = true
+      events       = ["commit_comment", "create", "delete", "fork", "gollum", "issue_comment", "issues", "member", "public", "pull_request", "pull_request_review_comment", "push", "release", "status", "team_add", "watch"]
+    }
+  ]
+
+  repository-issue_labels = concat(var.default-issue_labels)
+}
